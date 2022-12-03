837f142ad98b945fdbcfaeb657fbb41a0ce6b2d4
PucklaJ <jonaas.pucher000000@gmail.com>
2022-12-03 23:21:47 +0100

    Initial Commit

---
+++ src/setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup
+from setuptools.extension import Extension
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+dynareadout_dir = os.path.join(this_dir, 'dynareadout')
+
+dynareadout = Extension(
+  name='dynareadout',
+  include_dirs=[
+    os.path.join(this_dir, 'include'),
+    os.path.join(dynareadout_dir, 'src'),
+    os.path.join(dynareadout_dir, 'src', 'cpp')
+  ],
+  sources=[
+    # C Source Files
+    os.path.join(dynareadout_dir, 'src', 'binary_search.c'),
+    os.path.join(dynareadout_dir, 'src', 'binout.c'),
+    os.path.join(dynareadout_dir, 'src', 'binout_directory.c'),
+    os.path.join(dynareadout_dir, 'src', 'binout_glob.c'),
+    os.path.join(dynareadout_dir, 'src', 'd3_buffer.c'),
+    os.path.join(dynareadout_dir, 'src', 'd3plot.c'),
+    os.path.join(dynareadout_dir, 'src', 'd3plot_data.c'),
+    os.path.join(dynareadout_dir, 'src', 'd3plot_state.c'),
+    os.path.join(dynareadout_dir, 'src', 'path.c'),
+    os.path.join(dynareadout_dir, 'src', 'path_view.c'),
+    # C++ Source Files
+    os.path.join(dynareadout_dir, 'src', 'cpp', 'binout.cpp'),
+    os.path.join(dynareadout_dir, 'src', 'cpp', 'd3plot.cpp'),
+    os.path.join(dynareadout_dir, 'src', 'cpp', 'd3plot_part.cpp'),
+    # C++ Source Files for pybind11 module
+    os.path.join(dynareadout_dir, 'src', 'python', 'pybind11_binout.cpp'),
+    os.path.join(dynareadout_dir, 'src', 'python', 'pybind11_d3plot.cpp'),
+    os.path.join(dynareadout_dir, 'src', 'python', 'pybind11_module.cpp'),
+  ]
+)
+
+setup(
+  name='dynareadout',
+  version='4.0',
+  author='PucklaJ',
+  description='Blah Blah',
+  long_description='file: dynareadout/README.md',
+  url='https://github.com/PucklaJ/dynareadout',
+  classifiers=[
+    
+  ],
+  ext_modules=[dynareadout],
+  zip_safe=False,
+  include_package_data=True
+)
