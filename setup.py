e434dd52036be1b83e96adda28306a4fea99dbb0
PucklaJ <jonaas.pucher000000@gmail.com>
2022-12-04 15:31:18 +0100

    Add pybind11 as submodule

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dynareadout_dir = os.path.join('src', 'dynareadout')
 dynareadout = Extension(
   name='dynareadout',
   include_dirs=[
-    os.path.join(this_dir, 'src', 'include'),
+    os.path.join(this_dir, 'src', 'pybind11' , 'include'),
     os.path.join(dynareadout_dir, 'src'),
     os.path.join(dynareadout_dir, 'src', 'cpp')
   ],
