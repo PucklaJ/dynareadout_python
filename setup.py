0674ac59c30c2abdc1ae5512869d12473f9de3e1
PucklaJ <jonaas.pucher000000@gmail.com>
2023-11-23 13:37:00 +0100

    Update dynareadout to 23.11

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ dynareadout = Extension(
 )
 
 setup(name='dynareadout',
-      version='23.10.2',
+      version='23.11',
       ext_modules=[dynareadout],
       zip_safe=False,
       include_package_data=True,
