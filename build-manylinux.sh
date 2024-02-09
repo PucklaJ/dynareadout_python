94d9fd02430fc60a9265a51d7d41b7689e6de1e1
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-09 14:29:49 +0100

    Fix path to pip

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -5,5 +5,5 @@ set -xe
 git clone --recursive https://github.com/PucklaJ/dynareadout_python.git
 cd dynareadout_python
 
-DRO_PIP=/opt/python/cp310-cp310/pip DRO_OUTPUT=/io ./build.sh
+DRO_PIP=/opt/python/cp310-cp310/bin/pip DRO_OUTPUT=/io ./build.sh
 
