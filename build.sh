2dfce051e0b5f208c3341049582011bb8ce3d79c
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-09 15:50:51 +0100

    Add plat to build.sh

--- build.sh
+++ build.sh
@@ -9,10 +9,13 @@ fi
 if [[ ! -v DRO_OUTPUT ]]; then
     DRO_OUTPUT=$script_dir/dist
 fi
+if [[ ! -v PLAT ]]; then
+    PLAT=linux_x86_64
+fi
 
 rm -rf $script_dir/build $script_dir/dist $script_dir/*.egg-info
 
-$DRO_PIP wheel $script_dir --no-deps -w $DRO_OUTPUT
+$DRO_PIP wheel $script_dir --no-deps -w $DRO_OUTPUT --build-option "--plat-name=$PLAT"
 rm -rf $script_dir/build $script_dir/*.egg-info
 
 echo Successfully built wheel for dynareadout "($DRO_PIP)"
