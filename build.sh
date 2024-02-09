b27e02c4f8e77da25daf7aa1b210c9107bbd4757
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-09 16:45:46 +0100

    Remove --build-option from build.sh

--- build.sh
+++ build.sh
@@ -9,13 +9,10 @@ fi
 if [[ ! -v DRO_OUTPUT ]]; then
     DRO_OUTPUT=$script_dir/dist
 fi
-if [[ ! -v PLAT ]]; then
-    PLAT=linux_x86_64
-fi
 
 rm -rf $script_dir/build $script_dir/dist $script_dir/*.egg-info
 
-$DRO_PIP wheel $script_dir --no-deps -w $DRO_OUTPUT --build-option "--plat-name=$PLAT"
+$DRO_PIP wheel $script_dir --no-deps -w $DRO_OUTPUT
 rm -rf $script_dir/build $script_dir/*.egg-info
 
 echo Successfully built wheel for dynareadout "($DRO_PIP)"
