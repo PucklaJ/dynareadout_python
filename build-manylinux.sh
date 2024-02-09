63d05080a260adb92a294a2e583a05f5f1318aa2
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-09 16:03:51 +0100

    Build wheels into temp dir

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -5,19 +5,23 @@ set -xe
 git clone --recursive https://github.com/PucklaJ/dynareadout_python.git
 cd dynareadout_python
 
+mkdir -p /wheelhouse
+
 if [[ ! -v DRO_SINGLE ]]; then
     for PYBIN in /opt/python/*/bin; do
-        DRO_PIP=$PYBIN/pip DRO_OUTPUT=/io ./build.sh
+        DRO_PIP=$PYBIN/pip DRO_OUTPUT=/wheelhouse ./build.sh
     done
 else
-    DRO_OUTPUT=/io ./build.sh
+    DRO_OUTPUT=/wheelhouse ./build.sh
 fi
 
-for whl in /io/*.whl: do
-    if ! auditwheel show $whl; then
+for whl in /wheelhouse/*.whl: do
+    if [ ! auditwheel show $whl ]; then
         echo "Skipping non-platform wheel $whl"
     else
         auditwheel repair $whl --plat $PLAT -w /io
     fi
 done
+
+cp /wheelhouse/* /io/
 
