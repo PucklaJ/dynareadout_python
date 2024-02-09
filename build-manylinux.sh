97983d96fe04424f4051e090d7d6093406cb48c2
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-09 15:15:58 +0100

    Add single build for manylinux

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -5,7 +5,11 @@ set -xe
 git clone --recursive https://github.com/PucklaJ/dynareadout_python.git
 cd dynareadout_python
 
-for PYBIN in /opt/python/*/bin; do
-    DRO_PIP=$PYBIN/pip DRO_OUTPUT=/io ./build.sh
-done
+if [[ ! -v DRO_SINGLE ]]; then
+    for PYBIN in /opt/python/*/bin; do
+        DRO_PIP=$PYBIN/pip DRO_OUTPUT=/io ./build.sh
+    done
+else
+    DRO_OUTPUT=/io ./build.sh
+fi
 
