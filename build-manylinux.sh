8b9244f2328acd08ece57f6bf23e18a2fff90fd7
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-16 15:03:18 +0100

    Make build-manylinux.sh absolute

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -1,15 +1,16 @@
 #! /bin/sh
 
 set -xe
+script_dir=$(dirname $0)
 
 mkdir -p /wheelhouse
 
 if [[ ! -v DRO_SINGLE ]]; then
     for PYBIN in /opt/python/*/bin; do
-        DRO_PIP=$PYBIN/pip DRO_OUTPUT=/wheelhouse ./build.sh
+        DRO_PIP=$PYBIN/pip DRO_OUTPUT=/wheelhouse $script_dir/build.sh
     done
 else
-    DRO_OUTPUT=/wheelhouse ./build.sh
+    DRO_OUTPUT=/wheelhouse $script_dir/build.sh
 fi
 
 for whl in /wheelhouse/*.whl; do
