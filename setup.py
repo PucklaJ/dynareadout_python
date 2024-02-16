40c9375fcfb89769a873b2875c67d422bbc04528
PucklaJ <jonaas.pucher000000@gmail.com>
2024-02-16 23:53:28 +0100

    Bump version to 24.1.5

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ dynareadout = Extension(
 )
 
 setup(name='dynareadout',
-      version='24.1.4',
+      version='24.1.5',
       ext_modules=[dynareadout],
       zip_safe=False,
       include_package_data=True,
