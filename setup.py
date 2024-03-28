50c86c8278c10f89e5ccdf16b3b6c39c2f790cae
PucklaJ <jonaas.pucher000000@gmail.com>
2024-03-28 14:39:25 +0100

    Fix workflow

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ dynareadout = Extension(
 )
 
 setup(name='dynareadout',
-      version='24.03.0',
+      version='24.03.2',
       ext_modules=[dynareadout],
       zip_safe=False,
       include_package_data=True,
