4be5c03951f0fd0daad2f08dadb04d941c5a692b
PucklaJ <jonaas.pucher000000@gmail.com>
2024-04-26 08:16:00 +0200

    Bump version to 24.04

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ dynareadout = Extension(
 )
 
 setup(name='dynareadout',
-      version='24.03',
+      version='24.04',
       ext_modules=[dynareadout],
       zip_safe=False,
       include_package_data=True,
